ccaa85303abb2d5af5e2920ab0f4ac6ceddab47b
asrarbutt <asrarbutt1986@gmail.com>
2022-07-12 14:42:24 +0200

    add all get requests

--- src/main/java/com/example/shopservicespring/shop/controller/ShopController.java
+++ src/main/java/com/example/shopservicespring/shop/controller/ShopController.java
@@ -1,9 +1,14 @@
 package com.example.shopservicespring.shop.controller;
 
+import com.example.shopservicespring.shop.order.Order;
+import com.example.shopservicespring.shop.product.Product;
 import com.example.shopservicespring.shop.service.ShopService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -19,4 +24,35 @@ public class ShopController {
     public String mainPage(){
         return shopService.mainPage();
     }
+
+    @GetMapping("products")
+    public List<Product> getProducts(){
+
+        return shopService.listProducts();
+    }
+
+    @GetMapping(path = "products/{id}")
+    public Product getProduct(@PathVariable int id){
+        return shopService.getProduct(id);
+    }
+
+@GetMapping("orders")
+    public List<Order> getOrders(){
+        return shopService.listOrders();
+    }
+
+    @GetMapping(path = "orders/{id}")
+    public Order getOrders(@PathVariable int id){
+
+        return shopService.getOrder(id);
+    }
+
+    @PostMapping("orders/{id}")
+    public Order postOrder(@RequestBody int id){
+
+
+        return shopService.getOrder(id);
+    }
+
+
 }
